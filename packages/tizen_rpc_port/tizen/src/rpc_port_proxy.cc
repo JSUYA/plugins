5420b723e16cdc94a159d338dd1b6d1deb5703d8
JunsuChoi <jsuya.choi@samsung.com>
2025-08-04 17:15:12 +0900

    [WIP] Support multi proxy

--- packages/tizen_rpc_port/tizen/src/rpc_port_proxy.cc
+++ packages/tizen_rpc_port/tizen/src/rpc_port_proxy.cc
@@ -7,6 +7,7 @@
 #include <rpc-port-parcel.h>
 
 #include <cstdint>
+#include <map>
 #include <utility>
 #include <vector>
 
@@ -32,10 +33,12 @@ flutter::EncodableMap CreateEncodableMap(const char* event,
 
 }  // namespace
 
-std::unique_ptr<FlEventSink> RpcPortProxyManager::event_sink_;
+std::map<rpc_port_proxy_h, std::unique_ptr<FlEventSink>>
+    RpcPortProxyManager::event_sinks_;
 
-void RpcPortProxyManager::Init(std::unique_ptr<FlEventSink> event_sink) {
-  event_sink_ = std::move(event_sink);
+void RpcPortProxyManager::Init(rpc_port_proxy_h handle,
+                               std::unique_ptr<FlEventSink> event_sink) {
+  event_sinks_[handle] = std::move(event_sink);
 }
 
 RpcPortResult RpcPortProxyManager::Connect(rpc_port_proxy_h handle,
@@ -74,7 +77,11 @@ void RpcPortProxyManager::OnConnectedEvent(const char* receiver,
   rpc_port_proxy_h handle = static_cast<rpc_port_proxy_h>(user_data);
   flutter::EncodableMap map =
       CreateEncodableMap("connected", receiver, port_name, handle);
-  event_sink_->Success(flutter::EncodableValue(map));
+  auto it = event_sinks_.find(handle);
+  if (it != event_sinks_.end()) {
+    const auto& event_sink = it->second;
+    event_sink->Success(flutter::EncodableValue(map));
+  }
 }
 
 void RpcPortProxyManager::OnDisconnectedEvent(const char* receiver,
@@ -85,7 +92,12 @@ void RpcPortProxyManager::OnDisconnectedEvent(const char* receiver,
   rpc_port_proxy_h handle = static_cast<rpc_port_proxy_h>(user_data);
   flutter::EncodableMap map =
       CreateEncodableMap("disconnected", receiver, port_name, handle);
-  event_sink_->Success(flutter::EncodableValue(map));
+  auto it = event_sinks_.find(handle);
+  if (it != event_sinks_.end()) {
+    const auto& event_sink = it->second;
+    event_sink->Success(flutter::EncodableValue(map));
+    event_sinks_.erase(it);
+  }
 }
 
 void RpcPortProxyManager::OnRejectedEvent(const char* receiver,
@@ -98,7 +110,12 @@ void RpcPortProxyManager::OnRejectedEvent(const char* receiver,
       CreateEncodableMap("rejected", receiver, port_name, handle);
   map[flutter::EncodableValue("error")] = flutter::EncodableValue(
       std::string(get_error_message(get_last_result())));
-  event_sink_->Success(flutter::EncodableValue(map));
+  auto it = event_sinks_.find(handle);
+  if (it != event_sinks_.end()) {
+    const auto& event_sink = it->second;
+    event_sink->Success(flutter::EncodableValue(map));
+    event_sinks_.erase(it);
+  }
 }
 
 void RpcPortProxyManager::OnReceivedEvent(const char* receiver,
@@ -108,18 +125,28 @@ void RpcPortProxyManager::OnReceivedEvent(const char* receiver,
 
   rpc_port_proxy_h handle = static_cast<rpc_port_proxy_h>(user_data);
   rpc_port_h port = nullptr;
+
+  FlEventSink* event_sink = nullptr;
+  auto it = event_sinks_.find(handle);
+  if (it == event_sinks_.end()) {
+    LOG_ERROR("Failed to get event sink. handle(%p)", handle);
+    return;
+  }
+
+  event_sink = it->second.get();
+
   int ret = rpc_port_proxy_get_port(handle, RPC_PORT_PORT_CALLBACK, &port);
   if (ret != RPC_PORT_ERROR_NONE) {
-    event_sink_->Error("rpc_port_proxy_get_port failed",
-                       std::string(get_error_message(ret)));
+    event_sink->Error("rpc_port_proxy_get_port failed",
+                      std::string(get_error_message(ret)));
     return;
   }
 
   rpc_port_parcel_h parcel = nullptr;
   ret = rpc_port_parcel_create_from_port(&parcel, port);
   if (ret != RPC_PORT_ERROR_NONE) {
-    event_sink_->Error("rpc_port_parcel_create_from_port failed",
-                       std::string(get_error_message(ret)));
+    event_sink->Error("rpc_port_parcel_create_from_port failed",
+                      std::string(get_error_message(ret)));
     return;
   }
 
@@ -128,8 +155,8 @@ void RpcPortProxyManager::OnReceivedEvent(const char* receiver,
   ret = rpc_port_parcel_get_raw(parcel, reinterpret_cast<void**>(&raw), &size);
   if (ret != RPC_PORT_ERROR_NONE) {
     rpc_port_parcel_destroy(parcel);
-    event_sink_->Error("rpc_port_parcel_get_raw failed",
-                       std::string(get_error_message(ret)));
+    event_sink->Error("rpc_port_parcel_get_raw failed",
+                      std::string(get_error_message(ret)));
     return;
   }
   std::vector<uint8_t> raw_data(raw, raw + size);
@@ -139,7 +166,7 @@ void RpcPortProxyManager::OnReceivedEvent(const char* receiver,
       CreateEncodableMap("received", receiver, port_name, handle);
   map[flutter::EncodableValue("rawData")] =
       flutter::EncodableValue(std::move(raw_data));
-  event_sink_->Success(flutter::EncodableValue(map));
+  event_sink->Success(flutter::EncodableValue(map));
 }
 
 }  // namespace tizen
